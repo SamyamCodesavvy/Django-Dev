7e76a77d87b6d693b87aa2f4c98b3ce3dcc8d0f9
SamyamCodesavvy <samyam0913@gmail.com>
2024-05-21 17:16:08 +0545

    initial upload

---
+++ challenges/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect #added
+from django.urls import reverse
+from django.template.loader import render_to_string
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": "Learn Django for at least 20 minutes every day!"
+}
+# Create your views here.
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    if (month > len(months)):
+        return HttpResponseNotFound("<b><h1>Invalid month</h1></b>")
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])  #constructs a string path that looks like .../challenges/janurary
+    return HttpResponseRedirect(redirect_path)
+
+def monthly_challenge(request, month): #placeholder in urls.py is month so pass month as other argument
+    try:
+        challenge_text = monthly_challenges[month]
+        return (render(request, "challenges/challenge.html", {
+            "text": challenge_text, 
+            "month_name": month.capitalize()
+        }))
+    except:
+        return HttpResponseNotFound("<i><h2>The month is invalid!!!</h2></i>")
